e23cc0122dc4794c893f7d608f269a1db0d8d25d
Andresa Marçal <andresamarques3107@gmail.com>
2023-04-25 11:35:11 -0300

    Atualização CategoriaModel

--- src/main/java/com/generation/sustentatech/model/Categoria.java
+++ src/main/java/com/generation/sustentatech/model/Categoria.java
@@ -1,9 +1,7 @@
 package com.generation.sustentatech.model;
 
 import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
